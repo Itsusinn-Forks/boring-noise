e01409626a15a987b0174d8c78b8181031c37309
wtf <wtf@devbox-win.syd.home>
2025-02-13 04:00:30 +1100

    iinit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+// Copyright (c) 2019 Cloudflare, Inc. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+//! Simple implementation of the client-side of the WireGuard protocol.
+//!
+//! <code>git clone https://github.com/cloudflare/boringtun.git</code>
+
+pub mod noise;
+
+#[cfg(not(feature = "mock-instant"))]
+pub(crate) mod sleepyinstant;
+
+/// Re-export of the x25519 types
+pub mod x25519 {
+    pub use x25519_dalek::{
+        EphemeralSecret, PublicKey, ReusableSecret, SharedSecret, StaticSecret,
+    };
+}
